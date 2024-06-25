925a16657a374a95dc4093117a3bf4949de7e817
CamiloTello002 <camilojtello5@gmail.com>
2024-06-25 13:08:29 -0500

    took a template for my portfolio

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById('mobile-menu').addEventListener('click', function () {
+    var navbarMenu = document.querySelector('.navbar-menu');
+    navbarMenu.classList.toggle('active');
+});
